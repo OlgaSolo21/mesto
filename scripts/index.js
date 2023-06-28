2cac4cf9e53d172272c5d2b3182f60fd544bcdea
Olga Solodovnikova <ossolodovnikova@yandex.ru>
2023-06-28 14:55:20 +0300

    bugfix rev

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 const profileEditButton = document.querySelector('.profile__edit-button')
 const popupEditButton = document.querySelector('.popup')
 const buttonCloseEditButton = popupEditButton.querySelector('.popup__close')
+const formElement = document.querySelector('.popup__form')
+const nameInput = formElement.querySelector('.popup__input_type_name')
+const jobInput = formElement.querySelector('.popup__input_type_job')
+const profileTitle = document.querySelector('.profile__title')
+const profileSubtitle = document.querySelector('.profile__subtitle')
 
 function addPopup() {
     popupEditButton.classList.add('popup_opened')
@@ -12,26 +17,13 @@ function closeEditPopup () {
     popupEditButton.classList.remove('popup_opened')
 }
 
-profileEditButton.addEventListener('click', addPopup)
-buttonCloseEditButton.addEventListener('click', closeEditPopup)
-
-
-const formElement = document.querySelector('.popup__form')
-const submitPopup = document.querySelector('.popup')
-const nameInput = formElement.querySelector('.popup__input_name')
-const jobInput = formElement.querySelector('.popup__input_job')
-const profileTitle = document.querySelector('.profile__title')
-const profileSubtitle = document.querySelector('.profile__subtitle')
-
-function closeSubmitPopup() {
-    submitPopup.classList.remove('popup_opened')
-}
-
 function handleFormSubmit (evt) {
     evt.preventDefault()
     profileTitle.textContent = nameInput.value
     profileSubtitle.textContent = jobInput.value
-    closeSubmitPopup()
+    closeEditPopup()
     }
 
+profileEditButton.addEventListener('click', addPopup)
+buttonCloseEditButton.addEventListener('click', closeEditPopup)
 formElement.addEventListener('submit', handleFormSubmit)
