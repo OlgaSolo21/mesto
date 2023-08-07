191bda60cdceee8fd0282df61d421c805b554ff4
Olga Solodovnikova <ossolodovnikova@yandex.ru>
2023-08-07 19:54:59 +0300

    feat: js - массив карточек, кнопка лайка, открытие/закрытие попап фото. css - стилизовала корзину

--- scripts/cardArray.js
+++ scripts/cardArray.js
@@ -1,26 +1,58 @@
 const initialCards = [
     {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        name: 'Калининград',
+        link: 'https://images.unsplash.com/photo-1531000164940-cde686a49bfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80'
     },
     {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: 'Алтайский край',
+        link: 'https://images.unsplash.com/photo-1596003903067-bf5762ad5c19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80'
     },
     {
         name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        link: 'https://images.unsplash.com/photo-1490879112094-281fea0883dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80'
+    },
+    {
+        name: 'Карачаево-Черкессия',
+        link: 'https://images.unsplash.com/photo-1635530043255-eb163c579d4c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+    },
+    {
+        name: 'Сочи',
+        link: 'https://images.unsplash.com/photo-1567245597540-5232c358e457?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80'
+    },
+    {
+        name: 'Санкт-Петербург',
+        link: 'https://images.unsplash.com/photo-1610045129185-a421e70e755f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=417&q=80'
+    },
+    {
+        name: 'Сочи',
+        link: 'https://images.unsplash.com/photo-1567245597540-5232c358e457?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80'
+    },
+    {
+        name: 'Санкт-Петербург',
+        link: 'https://images.unsplash.com/photo-1610045129185-a421e70e755f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=417&q=80'
     }
 ];
+// находим список ul
+const cardsElement = document.querySelector('.cards__elements')
+// находим template по id и получаем достум в контенту
+const template = document.querySelector('#cards').content
+//перебираем массив и добавляем карточки
+initialCards.forEach(function (elements) {
+    // находим и клонируем содержимое темплейта
+    const templateElement = template.querySelector('.cards__item').cloneNode(true)
+    // наполняем содержимым
+    templateElement.querySelector('.cards__image').src = elements.link
+    templateElement.querySelector('.cards__title').textContent = elements.name
+    // настраиваем кнопку лайк по тренажеру урока 8 тема 3
+    templateElement.querySelector('.cards__like').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('cards__like_active');
+    })
+    // отображаем на странице
+    cardsElement.append(templateElement)
+})
+
+
+
+
+
+
--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const profileEditButton = document.querySelector('.profile__edit-button')
-const popupEditButton = document.querySelector('.popup')
+const popupEditButton = document.querySelector('.popup_profile')
 const buttonCloseEditButton = popupEditButton.querySelector('.popup__close')
 const formElement = document.querySelector('.popup__form')
 const nameInput = formElement.querySelector('.popup__input_type_name')
@@ -7,16 +7,26 @@ const jobInput = formElement.querySelector('.popup__input_type_job')
 const profileTitle = document.querySelector('.profile__title')
 const profileSubtitle = document.querySelector('.profile__subtitle')
 
+// константы кнопки addbutton 5пр
+const addButtonProfile = document.querySelector('.profile__add-button')
+const addPopupButton = document.querySelector('.popup_add')
+const closePopupButton = addPopupButton.querySelector('.popup_button')
+const addFormElement = document.querySelector('.popup_profile')
+
+//переменные для добавления фото НЕ СДЕЛАНО
+const addCardNew = document.querySelector('.cards__item')
+const placeInput = addFormElement.querySelector('.popup__input_type_place')
+const linkInput = addFormElement.querySelector('.popup__input_type_link')
+
+// пишем функции кнопки добавления данных профиля 4пр
 function addPopup() {
     popupEditButton.classList.add('popup_opened')
     nameInput.value = profileTitle.textContent
     jobInput.value = profileSubtitle.textContent
 }
-
 function closeEditPopup () {
     popupEditButton.classList.remove('popup_opened')
 }
-
 function handleFormSubmit (evt) {
     evt.preventDefault()
     profileTitle.textContent = nameInput.value
@@ -24,6 +34,29 @@ function handleFormSubmit (evt) {
     closeEditPopup()
     }
 
+// пишем функции кнопки добавления фото 5пр
+// функция открытия попап кнопки добавления
+function openPopupAdd() {
+    addPopupButton.classList.add('popup_opened')
+}
+// функция закрытия попап кнопки добавления
+function closePopupAdd() {
+    addPopupButton.classList.remove('popup_opened')
+}
+// функция сохранения нового фото НЕ СДЕЛАНО
+function handleAddPopup(evt) {
+    evt.preventDefault()
+    placeInput.value = addCardNew
+    linkInput.value = addCardNew
+    initialCards.append(placeInput, linkInput)
+    closePopupAdd()
+}
+
 profileEditButton.addEventListener('click', addPopup)
 buttonCloseEditButton.addEventListener('click', closeEditPopup)
 formElement.addEventListener('submit', handleFormSubmit)
+
+// вешаем слушатель на кнопку добавления фото
+addButtonProfile.addEventListener('click', openPopupAdd)
+closePopupButton.addEventListener('click', closePopupAdd)
+addFormElement.addEventListener('submit', handleAddPopup)
